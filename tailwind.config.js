e38a4fd4ceac81aa817352268ffd8218ce6aa050
origindev0719 <origindev0719@gmail.com>
2023-09-16 13:27:10 +0200

    boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        raspberry: "#E30B5C",
+        Vintage: " #E5E5DB",
+      },
+      fontFamily: {
+        sans: ['Plus Jakarta Sans', 'sans-serif'],
+        'sans-2': ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
